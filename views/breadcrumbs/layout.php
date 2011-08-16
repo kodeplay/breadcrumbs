71010ad388a0c26c8605e595f8889aa9e43ed87c
Jimit Modi <jimit@kodeplay.com>
2011-08-16 17:53:40 +0530

    To add seperater before last breadcrumbs

--- views/breadcrumbs/layout.php
+++ views/breadcrumbs/layout.php
@@ -11,16 +11,17 @@
 <?php if ($c > ($conf['min_depth']-1)) : ?>
 <?php foreach ($breadcrumbs as $k=>$crumb) : ?>
 <?php if ($crumb->get_url() !== NULL AND count($breadcrumbs > 1)) :  ?>
-	<li>
+	<li> 
         <a href="<?php echo $crumb->get_url()?>"><?php echo $crumb->get_title()?></a> 
-        <?php if ($k < $c-1 || $k === $c-1 && $conf['last'] == TRUE) { ?>
+        <?php //if ($k < $c-1 || $k === $c && $conf['last'] == TRUE) { ?>
+        <?php if($k+1 !== $c || $conf['last'] == TRUE){ ?>
         <span class="sep"><?php echo $conf['sep']; ?></span>
         <?php } ?>
     </li>
 <?php else : ?>
     <li><?php echo $crumb->get_title()?></li>
 <?php endif; ?>
-<?php $c-- ?>
+<?php //$c-- ?>
 <?php endforeach; ?>
 <?php endif; ?>
 </ul>
